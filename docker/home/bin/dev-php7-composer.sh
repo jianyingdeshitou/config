947dffb187e8b66a16abb679857428a493bfa20a
xuli <1099248596@qq.com>
2017-11-15 15:49:42 +0800

    [all] 初始化项目，基于 laravel 5.5

---
+++ docker/home/bin/dev-php7-composer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run --rm -it -v `pwd`:/app dev-php7-d9 \
+    composer $@
---
+++ docker/home/bin/new-laravel-5.3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run --rm -it -v `pwd`:/app dev-php7-d9 \
+    composer create-project --prefer-dist laravel/laravel /app 5.3.*
