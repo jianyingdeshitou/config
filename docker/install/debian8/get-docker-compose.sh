a1aeb5980d03ee5cc47148884d6122fb1b174765
坚硬的石头 <do_nb@outlook.com>
2018-07-22 11:42:51 +0800

    [docker] 修改 docker-compose 安装脚本

--- docker/install/debian8/get-docker-compose.sh
+++ docker/install/debian8/get-docker-compose.sh
@@ -1,3 +1,5 @@
 #/bin/bash
-curl -L https://github.com/docker/compose/releases/download/1.11.1/run.sh > /usr/local/bin/docker-compose
+#Install as a container
+# $1=1.21.2
+curl -L https://github.com/docker/compose/releases/download/$1/run.sh > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
